3a6d105856750af448cddbdd9e233e608b4b3296
HSW1005 <poohpama@naver.com>
2019-11-08 04:28:20 +0900

    hsw - overlay 닫기 기능도 추가했지롱.

--- js/overlaybutton.js
+++ js/overlaybutton.js
@@ -1,5 +1,5 @@
-document.getElementById("overlaybutton").addEventListener("click", windowopenPopup);
-function windowopenPopup(){
+document.getElementById("overlaybutton").addEventListener("click", overlayopen);
+function overlayopen(){
 
-	window.open('window.html', 'window팝업', 'width=600, height=200, menubar=yes, status=no, toolbar=yes, location=no, resizable=no')
+	window.open('window.html', 'window팝업', 'width=1000, height=80, menubar=yes, status=no, toolbar=yes, location=no, resizable=no')
 }
---
+++ js/overlayclose.js
@@ -0,0 +1,5 @@
+document.getElementById("closebutton").addEventListener("click", overlayclose);
+function overlayclose(){
+
+	window.close();
+}
