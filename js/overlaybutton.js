91c0015c2a1184316527d885263cd0ea85273dbe
HSW1005 <poohpama@naver.com>
2019-11-08 03:49:57 +0900

    hsw - collapsible 효과 추가 / overlay 버튼에 window.html 연결 / sidebar에서 웬일이 위에 마우스 ㄱㄱ

---
+++ js/overlaybutton.js
@@ -0,0 +1,5 @@
+document.getElementById("overlaybutton").addEventListener("click", windowopenPopup);
+function windowopenPopup(){
+
+	window.open('window.html', 'window팝업', 'width=600, height=200, menubar=yes, status=no, toolbar=yes, location=no, resizable=no')
+}
