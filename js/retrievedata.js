ae6632876e855d2029e5f1b653e4931be76451bf
dbsgus94 <rladbsgus94@gmail.com>
2019-11-08 18:30:38 +0900

    Collapsible

--- js/retrievedata.js
+++ js/retrievedata.js
@@ -1,13 +1,34 @@
-var showresult = "";
+var showresult = new Array(['']);
+var showresult_bus ="";
+
+
+
 
 window.onload = function() {
+  //sidebar
   chrome.storage.sync.get('headlines', function(result) { //retrieve data from chrome.storage
-    var element = document.getElementById("here");
-    
-        for (i = 0; i < result.headlines.title.length; i++) {
-          showresult += result.headlines.title[i] + "&nbsp&nbsp&nbsp&nbsp  |   &nbsp&nbsp&nbsp&nbsp";
-        }
-    
-        element.innerHTML = showresult;
+    var element = new Array();
+      for(let i =0; i<20; i++) {
+      element[i] = document.getElementById(i);
+      } 
+
+    for (let i = 0; i < result.headlines.title.length; i++) {
+      showresult[i] = result.headlines.title[i];
+    }
+      for(let i=0; i<20;i++) {
+      element[i].innerHTML = showresult[i];
+    }
   });
+  chrome.storage.sync.get('headlines', function(result) { //retrieve data from chrome.storage
+
+    var element_bus = document.getElementById("here2");
+
+        for (let i = 0; i < result.business.title.length; i++) {
+          showresult_bus += result.business.title[i] + "&nbsp&nbsp&nbsp&nbsp  |   &nbsp&nbsp&nbsp&nbsp";
+        }
+
+        element_bus.innerHTML = showresult_bus;
+  });
+
 }
+
