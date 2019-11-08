81ab693772c232525d5f438efa06c4c5a80f2d5a
jiwon_h98@naver.com <jiwon_h98@naver.com>
2019-11-09 01:55:38 +0900

    settimeout

--- settingclose.js
+++ settingclose.js
@@ -1,4 +1,9 @@
 document.getElementById("settingclosebutton").addEventListener("click", settingclose);
-function settingclose(){
+
+function settingclose(){   
+    setTimeout(close, 1500); 
+}
+
+function close() {
     window.location.replace("index.html");
 }
