b1bdd470600a3a754248ea48a219c3e6d672622e
HSW1005 <poohpama@naver.com>
2019-11-08 15:45:36 +0900

    hsw - 윤현형 코드 + 승우 코드

--- js/overlaybutton.js
+++ js/overlaybutton.js
@@ -1,5 +1,5 @@
 document.getElementById("overlaybutton").addEventListener("click", overlayopen);
 function overlayopen(){
 
-	window.open('window.html', 'window팝업', 'width=1500, height=80px, menubar=yes, status=no, toolbar=no, location=no, resizable=no')
+	window.open('window.html', '오버레이모드', 'width=1500, height=80px, toolbar=0, status=yes, menubars=0, scrollbars=0, resizable=0, location=0, directories=0');
 }
--- js/retrievetest.js
+++ js/retrievetest.js
@@ -2,7 +2,7 @@ var showresult = "";
 
 window.onload = function() {
   chrome.storage.sync.get('headlines', function(result) { //retrieve data from chrome.storage
-    var element = document.getElementById("here");
+    var element = document.getElementById("here2");
     
         for (i = 0; i < result.headlines.title.length; i++) {
           showresult += result.headlines.title[i] + "&nbsp&nbsp&nbsp&nbsp  |   &nbsp&nbsp&nbsp&nbsp";
