b9b594d530090088b9c4af2627ce4d4aa5a801fb
jiwon_h98@naver.com <jiwon_h98@naver.com>
2019-11-13 23:37:11 +0900

    overlaybutton.js 오류수정

--- js/overlaybutton.js
+++ js/overlaybutton.js
@@ -1,17 +1,25 @@
 document.getElementById("overlaybutton").addEventListener("click", overlayopen);
 function overlayopen(){
 	chrome.storage.local.get('contentscript', function(result) {
-		if(result.contentscript.value == 0) { //0이였으면 1로 바꾸고 
+		if(typeof result.contentscript == 'undefined') {
 			var contentscript = {value:1};
-			chrome.storage.local.set({contentscript:contentscript}, function() {
-				console.log('contentscript set to 1');
-			});
-		} else { //1이었으면 0으로 바꾼다. 
-			var contentscript = {value:0};
-			chrome.storage.local.set({contentscript:contentscript}, function() {
-				console.log('contentscript set to 0');
-			});
-		}
+				chrome.storage.local.set({contentscript:contentscript}, function() {
+					console.log('contentscript set to 1');
+				});
+		} else {
+			if(result.contentscript.value == 0) { //0이였으면 1로 바꾸고 
+				var contentscript = {value:1};
+				chrome.storage.local.set({contentscript:contentscript}, function() {
+					console.log('contentscript set to 1');
+				});
+			}
+			 else { //1이었으면 0으로 바꾼다. 
+				var contentscript = {value:0};
+				chrome.storage.local.set({contentscript:contentscript}, function() {
+					console.log('contentscript set to 0');
+				});
+			}
+		}	
 	});
 }
 
